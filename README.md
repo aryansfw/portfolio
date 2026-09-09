523242752c4840c3cb0fff9faf981fd598a22061
Aryan Shafa Wardana <aryan.s.wardana@gmail.com>
2026-09-09 21:19:01 +0700

    remove RSS feed

--- README.md
+++ README.md
@@ -4,7 +4,6 @@ This is a porfolio site template complete with a blog. Includes:
 
 - MDX and Markdown support
 - Optimized for SEO (sitemap, robots, JSON-LD schema)
-- RSS Feed
 - Dynamic OG images
 - Syntax highlighting
 - Tailwind v4
@@ -13,7 +12,7 @@ This is a porfolio site template complete with a blog. Includes:
 
 ## Demo
 
-https://aryanwardana.vercel.app
+https://aryanshafawardana.vercel.app
 
 ## How to Use
 
--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,20 +1,3 @@
-function ArrowIcon() {
-  return (
-    <svg
-      width="12"
-      height="12"
-      viewBox="0 0 12 12"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z"
-        fill="currentColor"
-      />
-    </svg>
-  )
-}
-
 export default function Footer() {
   return (
     <footer className="mb-16">
@@ -39,13 +22,6 @@ export default function Footer() {
           LinkedIn
         </a>
       </nav>
-      <a
-        className="mt-6 flex items-center text-sm text-neutral-600 transition-all hover:text-neutral-800 dark:text-neutral-300 dark:hover:text-neutral-100"
-        href="/rss"
-      >
-        <ArrowIcon />
-        <span className="ml-2">RSS</span>
-      </a>
       <p className="mt-2 text-neutral-600 dark:text-neutral-300">
         &copy; {new Date().getFullYear()} Aryan
       </p>
